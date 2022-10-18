cd3676ec2f6842aa130c1d162464c98f7b4c2c12
murasaki <yankvasya@gmail.com>
2022-10-18 19:29:57 +0400

    feat: [components/CustomElement] create CustomElement component

---
+++ src/components/CustomElement.vue
@@ -0,0 +1,31 @@
+<template>
+  <a @click.prevent="clickElement()" href="#" class="element">
+    {{ element.name }}
+  </a>
+</template>
+
+<script setup>
+const { element } = defineProps({
+  element: {
+    type: Object,
+  },
+});
+
+const emit = defineEmits(["update:element"]);
+
+const clickElement = () => emit("update:element", element);
+</script>
+
+<style scoped lang="scss">
+.element {
+  display: inline-flex;
+  border: 4px solid black;
+  padding: 3px;
+  transition: 0.2s all;
+
+  &:hover {
+    transform: scale(1.2);
+    transition: 0.3s all;
+  }
+}
+</style>
